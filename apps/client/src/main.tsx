4999fd50d819b1c63fa2bf22504407e1a1f49b6c
tom-ads <thomas_adams@hotmail.co.uk>
2022-10-19 12:18:38 +0100

    Client: fix build

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-import './index.css'
+import './styling/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
