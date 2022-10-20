9b5c4134b9d1a8aab2fcb13ca1c6bd78e725fa80
tom-ads <thomas_adams@hotmail.co.uk>
2022-10-21 00:19:57 +0100

    Actions: test output envs

--- apps/client/.profile.d/gen-htpasswd.sh
+++ apps/client/.profile.d/gen-htpasswd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-echo -e "test:$apr1$dwn/XmDn$mzSv1N89o/Bp7Ch7Dhc6U1" > /app/config/basic.htpasswd
+echo -e "${USERNAME}:$(perl -le 'print crypt($ENV{"PASSWORD"}, rand(0xffffffff));')" > /app/config/basic.htpasswd
