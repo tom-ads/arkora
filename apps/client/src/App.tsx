8768fd895eef5397a44f94490887b0244f165cc6
tom-ads <thomas_adams@hotmail.co.uk>
2022-10-23 14:26:44 +0100

    feat: setup styleguide into tailwind config

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,5 +1,5 @@
 const App = (): JSX.Element => {
-  return <div className="bg-red-500 min-h-full w-full"></div>
+  return <div className=" bg-gray-90 min-h-full w-full"></div>
 }
 
 export default App
--- apps/client/tailwind.config.cjs
+++ apps/client/tailwind.config.cjs
@@ -2,6 +2,37 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      purple: {
+        90: '#352C98',
+        80: '#564CCD',
+        70: '#7F75E9',
+        10: '#DFDDF5',
+      },
+      gray: {
+        90: '#121926',
+        80: '#202939',
+        70: '#364152',
+        60: '#4B5565',
+        50: '#697586',
+        40: '#9AA3B2',
+        30: '#C5CDD8',
+        20: '#DADFE7',
+        10: '#F2F4F8',
+      },
+      green: {
+        90: '#5B8A53',
+        10: '#E1F4DE',
+      },
+      red: {
+        90: '#B63A44',
+        10: '#FBDFDF',
+      },
+      gold: {
+        90: '#C07A02',
+        10: '#FBF1E0',
+      },
+    },
     extend: {},
   },
   plugins: [],
