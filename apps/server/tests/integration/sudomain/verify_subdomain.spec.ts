65ff1bce27e904bd1c93fb8e1cd799e46837a226
tom-ads <thomas_adams@hotmail.co.uk>
2022-10-25 22:46:24 +0100

    fix: verify subdomain route using origin, instead of param

--- apps/server/tests/integration/sudomain/verify_subdomain.spec.ts
+++ apps/server/tests/integration/sudomain/verify_subdomain.spec.ts
@@ -1,13 +1,11 @@
 import { test } from '@japa/runner'
-import { staticAppHostname } from 'Config/app'
 import { OrganisationFactory } from 'Database/factories'
 
 test.group('Verify Subdomain', () => {
   test('valid organisation subdomain should be verified', async ({ client }) => {
     OrganisationFactory.merge({ subdomain: 'test-org' }).create()
 
-    const origin = `http://test-org.${staticAppHostname}`
-    const response = await client.get('/subdomain').headers({ origin })
+    const response = await client.get('/subdomain/test-org')
 
     response.assertStatus(200)
   })
@@ -15,20 +13,9 @@ test.group('Verify Subdomain', () => {
   test('invalid organisation should not be verified', async ({ client }) => {
     OrganisationFactory.merge({ subdomain: 'test-org' }).create()
 
-    const origin = `http://diff-org.${staticAppHostname}`
-    const response = await client.get('/subdomain').headers({ origin })
+    const response = await client.get('/subdomain/diff-org')
 
     response.assertStatus(404)
     response.assertBody({ message: 'No organisation found' })
-  })
-
-  test('origin without a subdomain should not be verified', async ({ client }) => {
-    OrganisationFactory.merge({ subdomain: 'test-org' }).create()
-
-    const origin = `http://${staticAppHostname}`
-    const response = await client.get('/subdomain').headers({ origin })
-
-    response.assertStatus(404)
-    response.assertBody({ message: 'No subdomain found' })
   })
 })
