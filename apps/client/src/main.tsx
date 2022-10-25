8cc44c21c2bb84f706875617d2cab38995cf5366
tom-ads <thomas_adams@hotmail.co.uk>
2022-10-25 01:19:38 +0100

    feat: add frontend validation and reusable form component

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import AppRouter from './routes'
+import { store } from './stores/store'
 
 import './styling/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App>
-        <AppRouter />
-      </App>
+      <Provider store={store}>
+        <App>
+          <AppRouter />
+        </App>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
