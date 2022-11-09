f0e07d736c1ace5af3ec42921cfeec1d1859b8b1
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-09 22:21:46 +0000

    feat: added debounced hook and input

---
+++ apps/client/src/components/Forms/DebouncedInput/index.tsx
@@ -0,0 +1,50 @@
+import { useDebounce } from '@/hooks/useDebounce'
+import classNames from 'classnames'
+import { useEffect, useState } from 'react'
+
+type FormDebouncedInputBaseProps = {
+  name: string
+  placeHolder?: string
+  size?: 'sm' | 'md' | 'lg'
+  error?: boolean
+  value: string
+  onChange: (val: string) => void
+}
+
+export const FormDebouncedInput = ({
+  name,
+  size = 'sm',
+  error,
+  placeHolder,
+  value,
+  onChange,
+}: FormDebouncedInputBaseProps): JSX.Element => {
+  const [input, setInput] = useState<string>(value)
+
+  const debouncedInput = useDebounce<string>(input, 500)
+
+  useEffect(() => {
+    onChange(debouncedInput)
+  }, [debouncedInput])
+
+  return (
+    <input
+      type="text"
+      name={name}
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      placeholder={placeHolder}
+      className={classNames(
+        'border border-gray-40 w-full rounded placeholder:text-green-60 font-normal text-gray-80 transition-all outline-none',
+        {
+          'px-3 py-2 text-sm focus:shadow-sm': size === 'sm',
+          'px-3 py-3 text-base focus:shadow-md': size === 'md',
+          'px-[0.875rem] py-[10px] text-xl focus:shadow-lg': size === 'lg',
+
+          'focus:shadow-purple-70 focus:border-purple-90': !error,
+          'border-red-90 focus:shadow-md focus:shadow-red-90': error,
+        },
+      )}
+    />
+  )
+}
