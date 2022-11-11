f8c8a349181fb115f12d5aa6522485fa7e362719
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-11 23:10:20 +0000

    fix: AuthLayout structure

--- apps/client/src/components/Layouts/AuthLayout/index.tsx
+++ apps/client/src/components/Layouts/AuthLayout/index.tsx
@@ -3,22 +3,24 @@ import { ArkoraLogo } from '@/components'
 
 export const AuthLayout = (): JSX.Element => {
   return (
-    <div className="relative min-h-full">
-      <div className="absolute inset-0 from-purple-90 -left-96 bg-gradient-radial"></div>
-      <div className="fixed w-1/3 inset-y-0 h-full right-0">
-        <ArkoraLogo className="w-[18.375rem] lg:h-[18.875rem] xl:w-[22.375rem] xl:h-[22.875rem] absolute -top-[4.3125rem] left-7" />
-        <ArkoraLogo className="lg:w-[14.1875rem] lg:h-[14.5rem] absolute -right-[6.5625rem] top-48" />
-        <ArkoraLogo className="lg:w-[14.1875rem] lg:h-[14.5rem] absolute -left-16 bottom-[23.4375rem]" />
-        <ArkoraLogo className="w-[18.375rem] lg:h-[18.875rem] xl:w-[22.375rem] xl:h-[22.875rem] absolute -bottom-[7.1875rem] -right-[6.5625rem]" />
-      </div>
-
-      <div className="fixed w-full lg:w-2/3 bg-white inset-y-0 h-full py-12 sm:px-[42px] px-4">
-        <div className="flex items-center gap-2 pb-3">
+    <div className="relative flex min-h-full">
+      <div className="w-full xl:w-2/3 bg-white sm:px-[42px] px-4 flex flex-col">
+        <div className="items-center gap-2 pb-3 hidden sm:flex pt-12">
           <ArkoraLogo className="w-12 h-12" />
           <p className="text-2xl text-gray-100 font-istokWeb font-normal">Arkora</p>
         </div>
 
-        <Outlet />
+        <main className="max-w-[500px] px-8 mx-auto flex-grow flex">
+          <Outlet />
+        </main>
+      </div>
+
+      <div className="w-1/3 hidden xl:block sticky overflow-hidden top-0">
+        <div className="absolute inset-0 from-purple-90 bg-gradient-radial"></div>
+        <ArkoraLogo className="max-w-[18.375rem] lg:max-h-[18.875rem] xl:max-w-[22.375rem] xl:max-h-[22.875rem] absolute -top-[4.3125rem] left-7" />
+        <ArkoraLogo className="lg:max-w-[14.1875rem] lg:max-h-[14.5rem] absolute -right-[6.5625rem] top-48" />
+        <ArkoraLogo className="lg:max-w-[14.1875rem] lg:max-h-[14.5rem] absolute -left-16 bottom-[23.4375rem]" />
+        <ArkoraLogo className="max-w-[18.375rem] lg:max-h-[18.875rem] xl:max-w-[22.375rem] xl:max-h-[22.875rem] absolute -bottom-[7.1875rem] -right-[6.5625rem]" />
       </div>
     </div>
   )
