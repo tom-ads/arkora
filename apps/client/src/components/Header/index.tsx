121167378d08f218f54fc5a6387ca5cf618d9491
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-12 22:07:49 +0000

    feat: add authenticated main nav to Header component

--- apps/client/src/components/Header/index.tsx
+++ apps/client/src/components/Header/index.tsx
@@ -1,15 +1,57 @@
+import { RootState } from '@/stores/store'
+import classNames from 'classnames'
+import { ReactNode } from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { Divider } from '../Divider'
 import { ArkoraLogo } from '../Icons'
 
+const NavItem = ({ to, children }: { to: string; children: ReactNode }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        classNames(
+          'font-medium text-sm px-3 py-1 rounded h-min min-w-[79px] grid place-content-center transition-colors',
+          {
+            'bg-white text-gray-100 hover:bg-gray-10': !isActive,
+            'bg-purple-10 text-purple-90': isActive,
+          },
+        )
+      }
+    >
+      <span>{children}</span>
+    </NavLink>
+  )
+}
+
 export const Header = (): JSX.Element => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
+
   return (
     <>
       <header className="w-full bg-white">
-        <div className="mx-9 py-4">
-          <div className="flex items-center gap-2">
+        <div className="mx-9 py-4 flex gap-x-6">
+          <div className="flex items-center gap-2 pr-3">
             <ArkoraLogo className="w-[38px] h-[39px]" />
             <p className="text-2xl text-gray-100 font-istokWeb font-normal">Arkora</p>
           </div>
+
+          {isAuthenticated && (
+            <nav className="flex gap-2 px-3 items-center">
+              <NavItem to="projects">Projects</NavItem>
+              <NavItem to="team">Team</NavItem>
+            </nav>
+          )}
         </div>
+
+        {isAuthenticated && (
+          <>
+            <Divider />
+
+            <div className="h-[51px] mx-9 flex gap-x-6"></div>
+          </>
+        )}
       </header>
     </>
   )
