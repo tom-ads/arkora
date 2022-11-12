1deab589e76748c44fc765b5ce682be2e48e2ce5
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-12 22:07:29 +0000

    fix: add xsrf header and resolve same-site cross-origin blocking set-cookie header

--- apps/client/src/api/index.ts
+++ apps/client/src/api/index.ts
@@ -1,12 +1,38 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+/* 
+  Certain web browsers treat SameSite: Strict and subdomains as
+  cross-origins when using localhost. So, we need to set the subdoamin
+  into the request URL.
+*/
+const baseUrl = (): string => {
+  if (import.meta.env.PROD) {
+    return import.meta.env.VITE_ARKORA_API_BASE_URL
+  }
+
+  const url = new URL(import.meta.env.VITE_ARKORA_API_BASE_URL)
+  url.hostname = window.location.hostname
+  return url.toString()
+}
+
+const prepareHeaders = (headers: Headers) => {
+  const xsrfCookie = document.cookie.match(new RegExp('(XSRF-TOKEN)=([^;]*)'))
+  if (xsrfCookie) {
+    headers.set('X-XSRF-TOKEN', decodeURIComponent(xsrfCookie[3]))
+  }
+  return headers
+}
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_ARKORA_API_BASE_URL,
+  baseUrl: baseUrl(),
+  credentials: 'include',
+  prepareHeaders: prepareHeaders,
 })
 
 const appApi = createApi({
   reducerPath: 'arkoraApi',
   baseQuery,
+
   endpoints: () => ({}),
 })
 
