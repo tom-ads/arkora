623f2e1ab12d15c880c11f8d544e85f2a5ab98fe
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-15 19:02:52 +0000

    feat: return budgets in index projects

--- apps/server/app/Controllers/Http/ProjectController.ts
+++ apps/server/app/Controllers/Http/ProjectController.ts
@@ -2,9 +2,13 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ProjectController {
   public async index(ctx: HttpContextContract) {
-    const projects = await ctx.organisation?.related('projects').query()
+    const projects = await ctx.organisation?.related('projects').query().preload('budgets')
+
     return {
-      projects: projects?.map((p) => p.serialize()),
+      projects: projects?.map((p) => ({
+        ...p.serialize(),
+        budgets: p.budgets.map((budget) => budget.serialize()),
+      })),
     }
   }
 }
