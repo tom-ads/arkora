8312baa2ab5c55194da349cde5d5ab5983c54844
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-24 01:37:41 +0000

    feat: add custom error messages to global error handler

--- apps/server/app/Exceptions/Handler.ts
+++ apps/server/app/Exceptions/Handler.ts
@@ -15,9 +15,26 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract): Promise<any> {
+    if (error.code === 'E_ROW_NOT_FOUND') {
+      return ctx.response.notFound({
+        message: 'Resource not found',
+      })
+    }
+
+    if (error.code === 'E_CANNOT_FIND_ROUTE') {
+      return ctx.response.notFound({
+        message: 'Route not found',
+      })
+    }
+
+    return super.handle(error, ctx)
   }
 }
