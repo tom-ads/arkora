bde191a48df93a09dde0fcecb9a4090cf8f5db10
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-30 01:42:22 +0000

    fix: input type components to match input styling

--- apps/client/src/components/Forms/TimeInput/index.tsx
+++ apps/client/src/components/Forms/TimeInput/index.tsx
@@ -1,31 +1,14 @@
-import classNames from 'classnames'
+import { VariantProps } from 'class-variance-authority'
+import { HTMLAttributes } from 'react'
 import { useFormContext } from 'react-hook-form'
+import { input } from '../Input'
 
-type FormTimeInputProps = {
+interface FormTimeInputProps extends HTMLAttributes<HTMLInputElement>, VariantProps<typeof input> {
   name: string
-  size?: 'sm' | 'md' | 'lg'
-  error?: boolean
 }
 
-export const FormTimeInput = ({ name, size = 'sm', error }: FormTimeInputProps): JSX.Element => {
+export const FormTimeInput = ({ name, size, error }: FormTimeInputProps): JSX.Element => {
   const { register } = useFormContext()
 
-  return (
-    <input
-      id={name}
-      type="time"
-      {...register(name)}
-      className={classNames(
-        'border border-gray-40 w-full rounded placeholder:text-gray-60 font-normal text-gray-80 transition-all outline-none appearance-none',
-        {
-          'px-3 py-2 text-sm focus:shadow-sm': size === 'sm',
-          'px-3 py-3 text-base focus:shadow-md': size === 'md',
-          'px-[0.875rem] py-[10px] text-xl focus:shadow-lg': size === 'lg',
-
-          'focus:shadow-purple-70 focus:border-purple-90': !error,
-          'border-red-90 focus:shadow-md focus:shadow-red-90': error,
-        },
-      )}
-    />
-  )
+  return <input id={name} type="time" className={input({ size, error })} {...register(name)} />
 }
