061294afbcf1a1f417febde07b631ce2f8c8c1d0
tom-ads <thomas_adams@hotmail.co.uk>
2022-11-30 02:25:35 +0000

    feat: view project integration test

---
+++ apps/server/tests/integration/project/view.spec.ts
@@ -0,0 +1,160 @@
+import { test } from '@japa/runner'
+import Status from 'App/Enum/Status'
+import UserRole from 'App/Enum/UserRole'
+import { OrganisationFactory, UserFactory } from 'Database/factories'
+
+test.group('Project: View Project', () => {
+  test('organisation manager can view a project', async ({ client, route }) => {
+    const authUser = await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'test-org' }).with('clients', 1, (clientBuilder) => {
+        return clientBuilder
+          .merge({ organisationId: clientBuilder.parent.id })
+          .with('projects', 1, (projectBuilder) => {
+            return projectBuilder.merge({ name: 'new-project' })
+          })
+      })
+    })
+      .with('role', 1, (roleBuilder) => roleBuilder.merge({ name: UserRole.OWNER }))
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+      .loginAs(authUser)
+
+    response.assertStatus(200)
+    response.assertBody({
+      id: 1,
+      name: 'new-project',
+      private: false,
+      show_cost: false,
+      status: Status.ACTIVE,
+    })
+  })
+
+  test('organisation org_admin can view a project', async ({ client, route }) => {
+    const authUser = await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'test-org' }).with('clients', 1, (builder) => {
+        return builder.with('projects', 1, (projectBuilder) => {
+          projectBuilder.merge({ name: 'new-project' })
+        })
+      })
+    })
+      .with('role', 1, (roleBuilder) => roleBuilder.merge({ name: UserRole.ORG_ADMIN }))
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+      .loginAs(authUser)
+
+    response.assertStatus(200)
+    response.assertBody({
+      id: 1,
+      name: 'new-project',
+      private: false,
+      show_cost: false,
+      status: Status.ACTIVE,
+    })
+  })
+
+  test('organisation owner can view a project', async ({ client, route }) => {
+    const authUser = await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'test-org' }).with('clients', 1, (builder) => {
+        return builder.with('projects', 1, (projectBuilder) => {
+          projectBuilder.merge({ name: 'new-project' })
+        })
+      })
+    })
+      .with('role')
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+      .loginAs(authUser)
+
+    response.assertStatus(200)
+    response.assertBody({
+      id: 1,
+      name: 'new-project',
+      private: false,
+      show_cost: false,
+      status: Status.ACTIVE,
+    })
+  })
+
+  test('organisation member cannot view a project', async ({ client, route }) => {
+    const authUser = await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'test-org' }).with('clients', 1, (builder) => {
+        return builder.with('projects', 1, (projectBuilder) => {
+          projectBuilder.merge({ name: 'new-project' })
+        })
+      })
+    })
+      .with('role', 1, (roleBuilder) => roleBuilder.merge({ name: UserRole.MEMBER }))
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+      .loginAs(authUser)
+
+    response.assertStatus(403)
+  })
+
+  test('unauthenticated user cannot view a project', async ({ client, route }) => {
+    await OrganisationFactory.merge({ subdomain: 'test-org' })
+      .with('clients', 1, (clientBuilder) => {
+        return clientBuilder.merge({ organisationId: clientBuilder.parent.id }).with('projects', 1)
+      })
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+
+    response.assertStatus(401)
+  })
+
+  test('diff organisation user, cannot view a project from test organisation', async ({
+    client,
+    route,
+  }) => {
+    await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'test-org' }).with('clients', 1, (clientBuilder) => {
+        return clientBuilder
+          .merge({ organisationId: clientBuilder.parent.id })
+          .with('projects', 1, (projectBuilder) => {
+            projectBuilder.merge({ name: 'new-project' })
+          })
+      })
+    })
+      .with('role')
+      .create()
+
+    const diffUser = await UserFactory.with('organisation', 1, (orgBuilder) => {
+      return orgBuilder.merge({ subdomain: 'diff-org' }).with('clients', 1, (builder) => {
+        return builder.with('projects', 1, (projectBuilder) => {
+          projectBuilder.merge({ name: 'new-project' })
+        })
+      })
+    })
+      .with('role')
+      .create()
+
+    const response = await client
+      .get(route('ProjectController.view', { project: 1 }))
+      .headers({ origin: `http://test-org.arkora.co.uk` })
+      .withCsrfToken()
+      .loginAs(diffUser)
+
+    response.assertStatus(404)
+    response.assertBody({ message: 'Organisation account does not exist' })
+  })
+})
