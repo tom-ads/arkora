b39493c7dfeb1e4d6e4035bc8f514f37d38fc93d
tom-ads <thomas_adams@hotmail.co.uk>
2023-02-03 23:55:39 +0000

    feat: basic currency formatter

---
+++ apps/client/src/components/FormatCurrency/index.tsx
@@ -0,0 +1,24 @@
+import { useMemo } from 'react'
+
+type FormatCurrencyProps = {
+  value: number | null
+  currency?: string
+}
+
+export const FormatCurrency = ({ value, currency = 'GBP' }: FormatCurrencyProps): JSX.Element => {
+  const formatter = useMemo(() => {
+    // Locale will default to users browser
+    return new Intl.NumberFormat(undefined, {
+      style: 'currency',
+      currency: currency,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    })
+  }, [currency])
+
+  if (!value) {
+    return <p>- - -</p>
+  }
+
+  return <span>{formatter.format(value)}</span>
+}
