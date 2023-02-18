3ca1db5f1ef11ecf7c38f85adfdfc6e40a87222e
tom-ads <thomas_adams@hotmail.co.uk>
2023-02-18 01:08:16 +0000

    fix: prevent Show/Hide tabbing on PasswordInput

--- apps/client/src/components/Forms/PasswordInput/index.tsx
+++ apps/client/src/components/Forms/PasswordInput/index.tsx
@@ -31,6 +31,7 @@ export const FormPasswordInput = ({
       <div className="absolute inset-y-0 right-[12px] flex items-center">
         <button
           type="button"
+          tabIndex={-1} // Prevent icon from being tabbable
           onClick={() => setShowPassword(!showPassword)}
           className="w-4 h-4 outline-none focus-visible:text-purple-90 hover:text-purple-90"
         >
