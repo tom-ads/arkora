9033ca3daaddd8847116cb339bbf328e4190e6e8
tom-ads <thomas_adams@hotmail.co.uk>
2023-04-14 00:12:17 +0100

    feat: budget note migration and model

---
+++ apps/server/app/Models/BudgetNote.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Budget from './Budget'
+
+export default class BudgetNote extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public note: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  // Relationships
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Budget)
+  public budget: BelongsTo<typeof Budget>
+}
