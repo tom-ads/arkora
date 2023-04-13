c0886f30ca47c85582b1a616c6acf2bbc03a1fd1
tom-ads <thomas_adams@hotmail.co.uk>
2023-04-14 00:11:35 +0100

    feat: ValidateSubdomain -> VerifyTenant + removed auth check

--- apps/server/app/Middleware/VerifyTenant.ts
+++ apps/server/app/Middleware/VerifyTenant.ts
@@ -1,23 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { getOriginSubdomain } from './../../helpers/subdomain'
+import { getOriginSubdomain } from '../../helpers/subdomain'
 
 /**
- *
  * Validate subdomain checks the organisation being requested
  * from the origin exists and is the auth users linked organisation.
  *
  * Subdomains are unique, so we can check against the name and not unique
  * primary key.
  */
-export default class ValidateSubdomain {
+export default class VerifyTenant {
   public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
-    // Check user is authenticated
-    const auth = await ctx.auth.check()
-    if (!auth || !ctx.auth?.user) {
-      ctx.response.unauthorized({ message: 'Unauthenticated' })
-      return
-    }
-
     // Check the origin header is present in the request
     if (!ctx.request.header('origin')) {
       ctx.response.badRequest({ message: 'Missing Origin header' })
@@ -31,8 +23,8 @@ export default class ValidateSubdomain {
       return
     }
 
-    // Check auth user organisation matches the request origin
-    const userOrg = await ctx.auth.user?.related('organisation').query().first()
+    // Check auth user organisation matches origin organisation
+    const userOrg = await ctx.auth?.user?.related('organisation').query().first()
     if (userOrg?.subdomain !== originSubdomain) {
       ctx.response.notFound({ message: 'Organisation account does not exist' })
       return
