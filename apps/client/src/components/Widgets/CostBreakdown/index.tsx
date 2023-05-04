a810ef7d4e3c808608f5d904cb95a73df640295d
tom-ads <thomas_adams@hotmail.co.uk>
2023-05-04 17:23:57 +0100

    fix: cost breakdown responsiveness

--- apps/client/src/components/Widgets/CostBreakdown/index.tsx
+++ apps/client/src/components/Widgets/CostBreakdown/index.tsx
@@ -76,82 +76,84 @@ export const CostBreakdownWidget = ({ value }: CostBreakdownWidgetProps): JSX.El
       </div>
 
       {/* Insights */}
-      <div className="grid grid-cols-3 grid-rows-1 divide-x divide-dashed divide-gray-30 px-[21px]">
-        <div className="pr-[22px] pt-[18px] pb-4">
-          <p className="font-medium text-gray-50 text-sm mb-3">Cost</p>
-          <div className="space-y-2">
-            <div className="space-y-[2px]">
-              <p className="text-sm text-gray-70">Allocated</p>
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-100">
-                  {formattedInsights.showCost ? (
-                    <FormatCurrency value={formattedInsights.allocatedCost} />
-                  ) : (
-                    <span>- - -</span>
-                  )}
-                </p>
-                <p className="text-sm font-medium text-gray-100">
-                  {formatMinutesToHourMinutes(formattedInsights.allocatedDuration)}
-                </p>
+      <div className="grid grid-cols-2 lg:grid-cols-3 grid-rows-1 divide-y lg:divide-y-0 lg:divide-x divide-dashed divide-gray-30 px-[21px]">
+        <div className="col-span-2 grid sm:grid-cols-2 divide-y sm:divide-y-0 sm:divide-x divide-dashed divide-gray-30">
+          <div className="pr-[22px] pt-[18px] pb-4">
+            <p className="font-medium text-gray-50 text-sm mb-3">Cost</p>
+            <div className="space-y-2">
+              <div className="space-y-[2px]">
+                <p className="text-sm text-gray-70">Allocated</p>
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-gray-100">
+                    {formattedInsights.showCost ? (
+                      <FormatCurrency value={formattedInsights.allocatedCost} />
+                    ) : (
+                      <span>- - -</span>
+                    )}
+                  </p>
+                  <p className="text-sm font-medium text-gray-100">
+                    {formatMinutesToHourMinutes(formattedInsights.allocatedDuration)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="space-y-[2px]">
+                <p className="text-sm text-gray-70">Used</p>
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-gray-100">
+                    {formattedInsights.showCost ? (
+                      <FormatCurrency value={formattedInsights.usedCost} />
+                    ) : (
+                      <span>- - -</span>
+                    )}
+                  </p>
+                  <p className="text-sm font-medium text-gray-100">
+                    {formatMinutesToHourMinutes(formattedInsights.usedDuration)}
+                  </p>
+                </div>
               </div>
             </div>
+          </div>
 
-            <div className="space-y-[2px]">
-              <p className="text-sm text-gray-70">Used</p>
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-100">
-                  {formattedInsights.showCost ? (
-                    <FormatCurrency value={formattedInsights.usedCost} />
-                  ) : (
-                    <span>- - -</span>
-                  )}
-                </p>
-                <p className="text-sm font-medium text-gray-100">
-                  {formatMinutesToHourMinutes(formattedInsights.usedDuration)}
-                </p>
+          <div className="lg:px-[22px] sm:pl-[22px] pt-[18px] pb-4">
+            <p className="font-medium text-gray-50 text-sm mb-3">Spent</p>
+            <div className="space-y-2">
+              <div className="space-y-[2px]">
+                <p className="text-sm text-gray-70">Billable</p>
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-green-90">
+                    {formattedInsights.showCost ? (
+                      <FormatCurrency value={formattedInsights.billableCost} />
+                    ) : (
+                      <span>- - -</span>
+                    )}
+                  </p>
+                  <p className="text-sm font-medium text-gray-100">
+                    {formatMinutesToHourMinutes(formattedInsights.billableDuration)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="space-y-[2px]">
+                <p className="text-sm text-gray-70">Non-billable</p>
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-medium text-red-90">
+                    {formattedInsights.showCost ? (
+                      <FormatCurrency value={formattedInsights.unbillableCost} />
+                    ) : (
+                      <span>- - -</span>
+                    )}
+                  </p>
+                  <p className="text-sm font-medium text-gray-100">
+                    {formatMinutesToHourMinutes(formattedInsights.unbillableDuration)}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="px-[22px] pt-[18px] pb-4">
-          <p className="font-medium text-gray-50 text-sm mb-3">Spent</p>
-          <div className="space-y-2">
-            <div className="space-y-[2px]">
-              <p className="text-sm text-gray-70">Billable</p>
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-green-90">
-                  {formattedInsights.showCost ? (
-                    <FormatCurrency value={formattedInsights.billableCost} />
-                  ) : (
-                    <span>- - -</span>
-                  )}
-                </p>
-                <p className="text-sm font-medium text-gray-100">
-                  {formatMinutesToHourMinutes(formattedInsights.billableDuration)}
-                </p>
-              </div>
-            </div>
-
-            <div className="space-y-[2px]">
-              <p className="text-sm text-gray-70">Non-billable</p>
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-red-90">
-                  {formattedInsights.showCost ? (
-                    <FormatCurrency value={formattedInsights.unbillableCost} />
-                  ) : (
-                    <span>- - -</span>
-                  )}
-                </p>
-                <p className="text-sm font-medium text-gray-100">
-                  {formatMinutesToHourMinutes(formattedInsights.unbillableDuration)}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="pl-[22px] pt-[18px] pb-4">
+        <div className="lg:pl-[22px] col-span-2 lg:col-span-1 pt-[18px] pb-4">
           <p className="font-medium text-gray-50 text-sm mb-3">Summary</p>
           <div className="divide-y divide-dashed divide-gray-30">
             <div className="flex items-center justify-between pb-2">
