925f65f5f4b5536434d69181cddb9c6e41bc93c1
tom-ads <thomas_adams@hotmail.co.uk>
2023-05-04 16:20:40 +0100

    fix: build

--- apps/client/src/features/project/components/Views/Budget/index.tsx
+++ apps/client/src/features/project/components/Views/Budget/index.tsx
@@ -1,19 +1,15 @@
 import { ManageBudgetModal } from '@/features/budget'
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { BudgetsTable } from '../../Tables'
 
 export const ProjectBudgetView = (): JSX.Element => {
   const [budgetId, setBudgetId] = useState<number | null>(null)
-
-  const { projectId } = useParams()
 
   return (
     <>
       <BudgetsTable onManage={(id) => setBudgetId(id)} />
 
       <ManageBudgetModal
-        projectId={projectId ? parseInt(projectId, 10) : null}
         budgetId={budgetId}
         isOpen={!!budgetId}
         onClose={() => setBudgetId(null)}
